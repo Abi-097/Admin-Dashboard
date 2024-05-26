a07ea8e7c1aca968aaf17e8a670dde3310e1c9ad
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-05-26 11:47:57 +0530

    inital added with data

---
+++ src/Data/Data.js
@@ -0,0 +1,114 @@
+import {
+  UilEstate,
+  UilClipboardAlt,
+  UilUsersAlt,
+  UilPackage,
+  UilChart,
+  UilSignOutAlt,
+  UilUsdSquare,
+  UilMoneyWithdrawal,
+} from "@iconscout/react-unicons";
+
+// Recent Card Imports
+import img1 from "../assets/imgs/img1.png";
+import img2 from "../assets/imgs/img2.png";
+import img3 from "../assets/imgs/img3.png";
+
+//Sidebar
+export const SidebarData = [
+  {
+    icon: UilEstate,
+    heading: "Dashboard",
+  },
+  {
+    icon: UilClipboardAlt,
+    heading: "Orders",
+  },
+  {
+    icon: UilUsersAlt,
+    heading: "Customers",
+  },
+  {
+    icon: UilPackage,
+    heading: "Products",
+  },
+  {
+    icon: UilChart,
+    heading: "Analytics",
+  },
+];
+
+//Cards
+export const cardsData = [
+  {
+    title: "Sales",
+    color: {
+      backGround: "linear-gradient(180deg, #bb67ff 0%, #c484f3 100%)",
+      boxShadow: "0px 10px 20px 0px #e0c6f5",
+    },
+    barValue: 70,
+    value: "25,970",
+    png: UilUsdSquare,
+    series: [
+      {
+        name: "Sales",
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+    ],
+  },
+  {
+    title: "Revenue",
+    color: {
+      backGround: "linear-gradient(180deg, #FF919D 0%, #FC929D 100%)",
+      boxShadow: "0px 10px 20px 0px #FDC0C7",
+    },
+    barValue: 80,
+    value: "14,270",
+    png: UilMoneyWithdrawal,
+    series: [
+      {
+        name: "Revenue",
+        data: [10, 100, 50, 70, 80, 30, 40],
+      },
+    ],
+  },
+  {
+    title: "Expenses",
+    color: {
+      backGround:
+        "linear-gradient(rgb(248, 212, 154) -146.42%, rgb(255 202 113) -46.42%)",
+      boxShadow: "0px 10px 20px 0px #F9D59B",
+    },
+    barValue: 60,
+    value: "4,270",
+    png: UilClipboardAlt,
+    series: [
+      {
+        name: "Expenses",
+        data: [10, 25, 15, 30, 12, 15, 20],
+      },
+    ],
+  },
+];
+
+// Recent Update Card Data
+export const UpdatesData = [
+  {
+    img: img1,
+    name: "Jackson",
+    noti: "has ordered Mac book M2.",
+    time: "10 seconds ago",
+  },
+  {
+    img: img2,
+    name: "Tony Stark",
+    noti: "has received Apple gadget for charging battery.",
+    time: "45 minutes ago",
+  },
+  {
+    img: img3,
+    name: "Black Widow",
+    noti: "has ordered Apple smart watch, Apple 2500mh battery backup Charger.",
+    time: "5 hours ago",
+  },
+];
